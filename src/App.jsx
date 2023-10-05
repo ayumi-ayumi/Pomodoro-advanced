f59e752e5fb25b8993164df5f1b7c41bc2bdaaaf
Ayumi <ayuayuayuayus@gmail.com>
2023-10-05 23:10:17 +0200

    creacting alarm audio function

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from 'react'
 import './App.css'
 import Calendar from 'react-calendar'
 import MyTimer from './components/MyTimer'
-import 'react-calendar/dist/Calendar.css'
+// import 'react-calendar/dist/Calendar.css'
+import alarm from '/Users/Ayumi/Desktop/SelfStudy/React/pomodoro-advanced/pomodoro-advanced/src/alarm.mp3'
+import cancel from '/Users/Ayumi/Desktop/SelfStudy/React/pomodoro-advanced/pomodoro-advanced/src/cancel.png'
 
 function App() {
   const time = new Date();
-  time.setSeconds(time.getSeconds() + 10); 
+  time.setSeconds(time.getSeconds() + 2); 
 
   const [value, setValue] = useState()
 
@@ -18,6 +20,8 @@ function App() {
         onClickDay={(e) => setValue(e)}
 
         />
+      <img src={cancel} />
+      <audio controls src={alarm} loop id="audio">現環境では終了時にアラームが鳴りません</audio>
       {/* <div>{value}</div> */}
     </>
   )
--- src/components/MyTimer.jsx
+++ src/components/MyTimer.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import { useTimer } from 'react-timer-hook';
+import 'react-calendar/dist/Calendar.css'
+import alarm from '/Users/Ayumi/Desktop/SelfStudy/React/pomodoro-advanced/pomodoro-advanced/src/alarm.mp3'
+
+
 
 export default function MyTimer({ expiryTimestamp }) {
   const {
@@ -16,14 +20,32 @@ export default function MyTimer({ expiryTimestamp }) {
   let displayMinutes = minutes.toString().padStart(2, '0')
   let displaySeconds = seconds.toString().padStart(2, '0')
 
+  const audio = new Audio({alarm});
+
+  // function playaudio(){
+  //   audio.play();
+  // }
+  
+  // function stopaudio(){
+  //   audio.pause();
+  // }
+
+
   return (
     <div style={{textAlign: 'center'}}>
       <div style={{fontSize: '100px'}}>
         <span>{displayMinutes}</span>:<span>{displaySeconds}</span>
       </div>
       {/* <p>{isRunning ? 'Running' : 'Not running'}</p> */}
+      {totalSeconds === 0 && audio.play()}
+      {/* {totalSeconds === 0 && <audio src={alarm} loop autoPlay id="audio"></audio>} */}
       <button onClick={start}>Start</button>
-      <button onClick={pause}>Pause</button>
+      <button onClick={() => {
+        audio.pause()
+        // pause()
+
+      }}>Pause</button>
+      {/* <button onClick={pause}>Pause</button> */}
       {/* <button onClick={resume}>Resume</button> */}
       <button onClick={() => {
         // Arrange the restart button into the reset button 
